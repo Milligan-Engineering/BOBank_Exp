b3af7ab942cd0bf573c55c41b938df336cd86239
Jeffrey Giesey <jjgiesey@milligan.edu>
2020-01-31 12:50:23 -0500

    Changed player number validation to while loop.

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -17,48 +17,50 @@ int main()
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    if (numberPlayers <= 6 && numberPlayers > 2)
-    {
-        startCash = 1500 / numberPlayers;
-        cout << "Each player will start with $" << startCash << " in cash. \n";
-       
-        cout << "Enter player 1 name: ";
-        cin >> Player1;
-        cout << "Enter player 2 name: ";
-        cin >> Player2;
-        cout << "Enter player 3 name: ";
-        cin >> Player3;
-        if (numberPlayers > 3)
-        {
-            cout << "Enter player 4 name: ";
-            cin >> Player4;
-            if (numberPlayers > 4)
-            {
-                cout << "Enter player 5 name: ";
-                cin >> Player5;
-                if (numberPlayers > 5)
-                {
-                    cout << "Enter player 6 name: ";
-                    cin >> Player6;
-                }
-            }
-        }
-        cout << "These are the players:\n";
-        cout << " 1: " << Player1 << endl;
-        cout << " 2: " << Player2 << endl;
-        cout << " 3: " << Player3 << endl;
-        cout << " 4: " << Player4 << endl;
-        cout << " 5: " << Player5 << endl;
-        cout << " 6: " << Player6 << endl;
-
-        cout << "Enter the maximum time per term in minute and seconds: ";
-        cin >> turnMinutes >> turnSeconds;
-        turnTime = turnMinutes + turnSeconds / secondsPerMinute;
-        cout << "Turn time is " << turnMinutes << " minutes and " << turnSeconds << " seconds or " << turnTime << " minutes.\n";
-    }
-    else
+    while ((numberPlayers > 6) || (numberPlayers < 3))
     {
         cout << "Sorry, number of players needs to be 3 to 6\n";
+        cout << "Enter the number of players: ";
+        cin >> numberPlayers;
     }
+    startCash = 1500 / numberPlayers;
+    cout << "Each player will start with $" << startCash << " in cash. \n";
+       
+    cout << "Enter player 1 name: ";
+    cin >> Player1;
+    cout << "Enter player 2 name: ";
+    cin >> Player2;
+    cout << "Enter player 3 name: ";
+    cin >> Player3;
+    if (numberPlayers > 3)
+    {
+        cout << "Enter player 4 name: ";
+        cin >> Player4;
+        if (numberPlayers > 4)
+        {
+            cout << "Enter player 5 name: ";
+            cin >> Player5;
+            if (numberPlayers > 5)
+            {
+                cout << "Enter player 6 name: ";
+                cin >> Player6;
+            }
+        }
+    }
+    cout << "These are the players:\n";
+    cout << " 1: " << Player1 << endl;
+    cout << " 2: " << Player2 << endl;
+    cout << " 3: " << Player3 << endl;
+    cout << " 4: " << Player4 << endl;
+    cout << " 5: " << Player5 << endl;
+    cout << " 6: " << Player6 << endl;
+
+    cout << "Enter the maximum time per term minutes: ";
+    cin >> turnMinutes;
+    cout << "Enter the maximum time per term seconds: ";
+    cin >> turnSeconds;
+    turnTime = turnMinutes + turnSeconds / secondsPerMinute;
+    cout << "Turn time is " << turnMinutes << " minutes and " << turnSeconds << " seconds or " << turnTime << " minutes.\n";
+ 
     return 0;
 }
