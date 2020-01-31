6922b81eed173420acf1894501e46e099569586c
Jeffrey Giesey <jjgiesey@milligan.edu>
2020-01-31 13:14:40 -0500

    Version 3b Incorporates good programming practices

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -1,5 +1,11 @@
-// BOBank_Exp.cpp : Version 03a
-// Incorporates conditional statements
+//File Name: BOBank_Exp.cpp
+//Author: JJ Giesey
+//Email Address: jjgiesey@milligan.edu
+//Term Project
+//Description: This program handles banking duties in the B&O Board Game. Class example version
+//Version 3b: Incorporates good programming practices
+//Last Changed: 02/04/2019
+
 
 #include <iostream>
 #include <string>
@@ -8,42 +14,53 @@ using namespace std;
 int numberPlayers, startCash;
 string Player1, Player2, Player3, Player4, Player5, Player6;
 int turnMinutes, turnSeconds;
-const double secondsPerMinute = 60;
+const int MIN_PLAYERS = 3;
+const int MAX_PLAYERS = 6;
+// const double secondsPerMinute = 60.0 Replaced constant with value 60.0
 double turnTime;
-
 
 int main()
 {
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    while ((numberPlayers > 6) || (numberPlayers < 3))
+    while ((numberPlayers > MAX_PLAYERS) || (numberPlayers < MIN_PLAYERS))
     {
-        cout << "Sorry, number of players needs to be 3 to 6\n";
+        cout << "Sorry, number of players needs to be " << MIN_PLAYERS << " to " << MAX_PLAYERS << ".\n";
         cout << "Enter the number of players: ";
         cin >> numberPlayers;
     }
-    startCash = 1500 / numberPlayers;
+    startCash = 1500 / numberPlayers; //Calculates starting cash based on number of players
     cout << "Each player will start with $" << startCash << " in cash. \n";
-       
+    
+    // Ask for the player names based on the number of players specified
     cout << "Enter player 1 name: ";
     cin >> Player1;
-    cout << "Enter player 2 name: ";
-    cin >> Player2;
-    cout << "Enter player 3 name: ";
-    cin >> Player3;
-    if (numberPlayers > 3)
+    /*While these nested if statments will work find they are kind of cumbersome and not 
+    too clear as to their function. When we implement arrays this will be much cleaner
+    with a while statement.*/
+    if (numberPlayers > 1)
     {
-        cout << "Enter player 4 name: ";
-        cin >> Player4;
-        if (numberPlayers > 4)
+        cout << "Enter player 2 name: ";
+        cin >> Player2;
+        if (numberPlayers > 2)
         {
-            cout << "Enter player 5 name: ";
-            cin >> Player5;
-            if (numberPlayers > 5)
+            cout << "Enter player 3 name: ";
+            cin >> Player3;
+            if (numberPlayers > 3)
             {
-                cout << "Enter player 6 name: ";
-                cin >> Player6;
+                cout << "Enter player 4 name: ";
+                cin >> Player4;
+                if (numberPlayers > 4)
+                {
+                    cout << "Enter player 5 name: ";
+                    cin >> Player5;
+                    if (numberPlayers > 5)
+                    {
+                        cout << "Enter player 6 name: ";
+                        cin >> Player6;
+                    }
+                }
             }
         }
     }
@@ -59,7 +76,7 @@ int main()
     cin >> turnMinutes;
     cout << "Enter the maximum time per term seconds: ";
     cin >> turnSeconds;
-    turnTime = turnMinutes + turnSeconds / secondsPerMinute;
+    turnTime = turnMinutes + turnSeconds / 60.0;
     cout << "Turn time is " << turnMinutes << " minutes and " << turnSeconds << " seconds or " << turnTime << " minutes.\n";
  
     return 0;
