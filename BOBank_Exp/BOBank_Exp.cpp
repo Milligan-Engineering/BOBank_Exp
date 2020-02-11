99c6f2e7598c0cdb61153e9baf80459c1e13d259
Jeffrey Giesey <jjgiesey@milligan.edu>
2020-02-11 13:53:50 -0500

    Initial version with some problems

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -2,9 +2,9 @@
 //Author: JJ Giesey
 //Email Address: jjgiesey@milligan.edu
 //Term Project
-//Description: This program handles banking duties in the B&O Board Game. Class example version
-//Version 3c: Incorporates Arrays
-//Last Changed: 02/05/2019
+//Description: This program handles banking duties in the B&O Board Game. Bad Loop Version
+//Version BL
+//Last Changed: 02/11/2020
 
 #include <iostream>
 #include <string>
@@ -12,7 +12,8 @@ using namespace std;
 
 const int MIN_PLAYERS = 3;
 const int MAX_PLAYERS = 6;
-int numberPlayers, startCash;
+int numberPlayers, startCash, a,b,c;
+string temp;
 string playerName[MAX_PLAYERS];
 int playerCash[MAX_PLAYERS];
 int turnMinutes, turnSeconds;
@@ -23,7 +24,7 @@ int main()
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    while ((numberPlayers > MAX_PLAYERS) || (numberPlayers < MIN_PLAYERS))
+    while ((numberPlayers < MAX_PLAYERS) || (numberPlayers > MIN_PLAYERS))
     {
         cout << "Sorry, number of players needs to be " << MIN_PLAYERS << " to " << MAX_PLAYERS << ".\n";
         cout << "Enter the number of players: ";
@@ -32,39 +33,40 @@ int main()
 
     // Enter player names
     int i=0; // Going to use "i" as a counter, exception to declaring before main
-    while (i < numberPlayers)
+    for (int i = 0; i < numberPlayers; i++)
     {
-        cout << "Enter player " << i+1 << " name: ";
-        cin >> playerName[i];
-        i++;
+        cout << "Enter player " << i << " name: ";
+        cin >> playerName[i-1];
     }
-    cout << "These are the players:\n";
-    int k = 0;
-    while (k < numberPlayers)
+    
+    //Sort names alphabetically (poorly)
+    for (int i = 0; i <= numberPlayers; i++)
     {
-        cout << k + 1 << ": " << playerName[k] << endl;
+        for (int i = 0; i <= numberPlayers - i; i++)
+        {
+            temp = playerName[i];
+            playerName[i] = playerName[i + 1];
+            playerName[i + 1] = temp;
+        }
+    }
+
+    cout << "These are the players:\n";
+    for (int k = 1; k <= numberPlayers; k++)
+    {
+        cout << k << ": " << playerName[k] << endl;
         k++;
     }
-   
+
     startCash = 1500 / numberPlayers; //Calculates starting cash based on number of players
     cout << "Each player will start with $" << startCash << " in cash. \n";
     // Initialize player cash
-    int c = 0;
-    while (c < numberPlayers)
+    for (int a = 0; b <= numberPlayers; c++);
     {
+        b = c + 1;
         playerCash[c] = startCash;
         cout << playerName[c] << " " << "will start with" << " $ " << playerCash[c] << endl ;
-        c++;
+        a = b - 1;
     }
-
-    /*
-    cout << "Enter the maximum time per term minutes: ";
-    cin >> turnMinutes;
-    cout << "Enter the maximum time per term seconds: ";
-    cin >> turnSeconds;
-    turnTime = turnMinutes + turnSeconds / 60.0;
-    cout << "Turn time is " << turnMinutes << " minutes and " << turnSeconds << " seconds or " << turnTime << " minutes.\n";
-    */
 
     return 0;
 }
