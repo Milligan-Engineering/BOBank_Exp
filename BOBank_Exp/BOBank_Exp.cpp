1b5c28189eae31bfe382872e844ff204a0f8bd41
Amperryman <Amperryman@my.milligan.edu>
2020-02-12 10:04:26 -0500

    this is fixed branch

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -24,7 +24,7 @@ int main()
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    while ((numberPlayers < MAX_PLAYERS) || (numberPlayers > MIN_PLAYERS))
+    while ((numberPlayers > MAX_PLAYERS) || (numberPlayers < MIN_PLAYERS))
     {
         cout << "Sorry, number of players needs to be " << MIN_PLAYERS << " to " << MAX_PLAYERS << ".\n";
         cout << "Enter the number of players: ";
@@ -32,40 +32,41 @@ int main()
     }
 
     // Enter player names
-    int i=0; // Going to use "i" as a counter, exception to declaring before main
+    // Going to use "i" as a counter, exception to declaring before main
     for (int i = 0; i < numberPlayers; i++)
     {
-        cout << "Enter player " << i << " name: ";
-        cin >> playerName[i-1];
+        cout << "Enter player " << i+1 << " name: ";
+        cin >> playerName[i];
     }
     
     //Sort names alphabetically (poorly)
-    for (int i = 0; i <= numberPlayers; i++)
+    for (int j = 0; j <= numberPlayers; j++)
     {
-        for (int i = 0; i <= numberPlayers - i; i++)
+        for (int i = 0; i <= numberPlayers - j; i++)
         {
+            if (playerName[i] > playerName[i + 1])
+            {
             temp = playerName[i];
             playerName[i] = playerName[i + 1];
             playerName[i + 1] = temp;
-        }
+       
+            }
+          }   
     }
 
     cout << "These are the players:\n";
-    for (int k = 1; k <= numberPlayers; k++)
+    for (int k = 0; k < numberPlayers; k++)
     {
-        cout << k << ": " << playerName[k] << endl;
-        k++;
+        cout << k+1 << ": " << playerName[k] << endl;
     }
 
     startCash = 1500 / numberPlayers; //Calculates starting cash based on number of players
     cout << "Each player will start with $" << startCash << " in cash. \n";
     // Initialize player cash
-    for (int a = 0; b <= numberPlayers; c++);
+    for (int c = 0; c <= numberPlayers; c++)
     {
-        b = c + 1;
         playerCash[c] = startCash;
         cout << playerName[c] << " " << "will start with" << " $ " << playerCash[c] << endl ;
-        a = b - 1;
     }
 
     return 0;
