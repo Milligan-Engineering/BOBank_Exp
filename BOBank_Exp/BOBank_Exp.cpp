2d0a88c4921646bf0c7060cd86ddb942b96473f3
Jeffrey Giesey <jjgiesey@milligan.edu>
2020-02-17 08:28:48 -0500

    still bad

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -24,7 +24,7 @@ int main()
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    while ((numberPlayers < MAX_PLAYERS) || (numberPlayers > MIN_PLAYERS))
+    while ((numberPlayers > MAX_PLAYERS) || (numberPlayers < MIN_PLAYERS))
     {
         cout << "Sorry, number of players needs to be " << MIN_PLAYERS << " to " << MAX_PLAYERS << ".\n";
         cout << "Enter the number of players: ";
@@ -35,26 +35,28 @@ int main()
     int i=0; // Going to use "i" as a counter, exception to declaring before main
     for (int i = 0; i < numberPlayers; i++)
     {
-        cout << "Enter player " << i << " name: ";
-        cin >> playerName[i-1];
+        cout << "Enter player " << i+1 << " name: ";
+        cin >> playerName[i];
     }
     
     //Sort names alphabetically (poorly)
-    for (int i = 0; i <= numberPlayers; i++)
+    for (int j = 0; j <= numberPlayers; j++)
     {
         for (int i = 0; i <= numberPlayers - i; i++)
         {
-            temp = playerName[i];
-            playerName[i] = playerName[i + 1];
-            playerName[i + 1] = temp;
+            if (playerName[i] > playerName[i + 1])
+            {
+                temp = playerName[i];
+                playerName[i] = playerName[i + 1];
+                playerName[i + 1] = temp;
+            }
         }
     }
 
     cout << "These are the players:\n";
     for (int k = 1; k <= numberPlayers; k++)
     {
-        cout << k << ": " << playerName[k] << endl;
-        k++;
+        cout << k << ": " << playerName[k-1] << endl;
     }
 
     startCash = 1500 / numberPlayers; //Calculates starting cash based on number of players
