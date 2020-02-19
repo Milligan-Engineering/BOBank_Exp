784c24541a4831a93912f547a09c9da7fa49a492
Jeffrey Giesey <jjgiesey@milligan.edu>
2020-02-19 10:08:01 -0500

    still good

--- BOBank_Exp/BOBank_Exp.cpp
+++ BOBank_Exp/BOBank_Exp.cpp
@@ -16,7 +16,8 @@ using namespace std;
 
 //Function Declaration
 int printPlayerInfo(int numberPlayersVal);
-//Precondition: The number of players is passed to function as an integer. Also there is data in arrays playerName and playerCash.
+//Precondition: The number of players is passed to function as an integer. Also there is data in arrays 
+//playerName and playerCash.
 //Postcondition: The player names and amount of their cash is printed out. The maximum abount of cash is returned.
 
 const int MIN_PLAYERS = 3;
@@ -44,13 +45,13 @@ int main()
     // Going to use "i" as a counter, exception to declaring before main
     for (int i = 0; i < numberPlayers; i++)
     {
-        cout << "Enter player " << i+1 << " name: ";
+        cout << "Enter player " << i + 1 << " name: ";
         cin >> playerName[i];
     }
-    
+
     //Function Call
     cout << "The maximum cash is: $" << printPlayerInfo(numberPlayers) << endl;
-   
+
     // Seed random variable
     long int currentTime = static_cast<long int>(time(0)); //Generate random seed
     srand(currentTime);
@@ -89,7 +90,7 @@ int main()
         playerCash[i] = startCash;
     }
 
- 
+
     //Function Call
     cout << "The maximum cash is: $" << printPlayerInfo(numberPlayers) << endl;
     return 0;
--- BOBank_Exp/BOBank_Exp02.cpp
+++ BOBank_Exp/BOBank_Exp02.cpp
@@ -1,48 +1,92 @@
-// BOBank_Exp.cpp : Version 02
-// Interger/Real Calculation Version
+//File Name: BOBank_Exp.cpp
+//Author: JJ Giesey
+//Email Address: jjgiesey@milligan.edu
+//Term Project
+//Description: This program handles banking duties in the B&O Board Game. Bad Loop Version
+//Version BL
+//Last Changed: 02/11/2020
 
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
+
+
 using namespace std;
 
-int numberPlayers, startCash;
-string Player1, Player2, Player3, Player4, Player5, Player6;
+const int MIN_PLAYERS = 3;
+const int MAX_PLAYERS = 60;
+int numberPlayers, startCash, iTemp;
+string temp;
+string playerName[MAX_PLAYERS];
+int playerCash[MAX_PLAYERS];
 int turnMinutes, turnSeconds;
-int secondsPerMinute = 60;
 double turnTime;
-
 
 int main()
 {
     cout << "Welcome to the B&O Banker Program\n";
     cout << "Enter the number of players: ";
     cin >> numberPlayers;
-    startCash = 1500 / numberPlayers;
-    cout << "Each player will start with $" << startCash << " in cash. \n";
-    cout << "Enter player 1 name: ";
-    cin >> Player1;
-    cout << "Enter player 2 name: ";
-    cin >> Player2;
-    cout << "Enter player 3 name: ";
-    cin >> Player3;
-    cout << "Enter player 4 name: ";
-    cin >> Player4;
-    cout << "Enter player 5 name: ";
-    cin >> Player5;
-    cout << "Enter player 6 name: ";
-    cin >> Player6;
-    cout << "These are the players:\n";
-    cout << " 1: " << Player1 << endl;
-    cout << " 2: " << Player2 << endl;
-    cout << " 3: " << Player3 << endl;
-    cout << " 4: " << Player4 << endl;
-    cout << " 5: " << Player5 << endl;
-    cout << " 6: " << Player6 << endl;
+    while ((numberPlayers > MAX_PLAYERS) || (numberPlayers < MIN_PLAYERS))
+    {
+        cout << "Sorry, number of players needs to be " << MIN_PLAYERS << " to " << MAX_PLAYERS << ".\n";
+        cout << "Enter the number of players: ";
+        cin >> numberPlayers;
+    }
 
-    cout << "Enter the maximum time per term is format minute space seconds: ";
-    cin >> turnMinutes >> turnSeconds;
-    turnTime = turnMinutes + turnSeconds / secondsPerMinute;
-    cout << "Turn time is " << turnTime << " minutes.\n";
+    // Enter player names
+    // Going to use "i" as a counter, exception to declaring before main
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        cout << "Enter player " << i+1 << " name: ";
+        cin >> playerName[i];
+    }
+    
+    cout << "These are the players:\n";
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        cout << i+1 << ": " << playerName[i] << endl;
+    }
+
+    //Assign random number to players
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        playerCash[i] = rand();
+    }
+
+    // Sort by random numbers
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        for (int j = 0; j < numberPlayers - i - 1; j++)
+        {
+            if (playerCash[j] > playerCash[j + 1])
+            {
+                temp = playerName[j];
+                iTemp = playerCash[j];
+                playerName[j] = playerName[j + 1];
+                playerCash[j] = playerCash[j + 1];
+                playerName[j + 1] = temp;
+                playerCash[j + 1] = iTemp;
+            }
+        }
+    }
+
+    cout << "These are the players (and random number) in random order:\n";
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        cout << i + 1 << ": " << playerName[i] << "("<<playerCash[i]<<")" << endl;
+    }
+
+
+    startCash = 1500 / numberPlayers; //Calculates starting cash based on number of players
+    cout << "Each player will start with $" << startCash << " in cash. \n";
+    // Initialize player cash
+    for (int i = 0; i < numberPlayers; i++)
+    {
+        playerCash[i] = startCash;
+        cout << playerName[i] << " " << "will start with" << " $ " << playerCash[i] << endl ;
+    }
 
     return 0;
 }
